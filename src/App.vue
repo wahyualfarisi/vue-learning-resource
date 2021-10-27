4632988b0ee02f9275ee902eb891d5a09db906ce
wahyu alfarisi <wahyualfarisi@gmail.com>
2021-10-27 22:59:22 +0700

    display resource

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
 <template>
-  <ul></ul>
+  <ul>
+      <learning-resource 
+        v-for="resource in storedResources" 
+        :key="resource.id"  
+        :title="resource.title"
+        :description="resource.description"
+        :link="resource.link"
+      ></learning-resource>
+  </ul>
 </template>
 
 <script>
+import LearningResource from './components/learning-resource/LearningResource.vue';
 export default {
+    components: {
+        LearningResource
+    },
     data(){
         return {
             storedResources: [
