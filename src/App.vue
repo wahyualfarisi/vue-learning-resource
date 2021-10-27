52e432c15c9d0d94c460a1dd594018dae62983ee
wahyu alfarisi <wahyualfarisi@gmail.com>
2021-10-27 22:50:14 +0700

    init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <ul></ul>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            storedResources: [
+                { id: 'Official-guide', title: 'Official Guide', description: 'The Official Vue.js Documentation', link: 'https://vuejs.org' },
+                { id: 'stack-overflow', title: 'Stack Overflow', description: 'The Site you can use to find anything in your problem', link: 'https://vuejs.org' },
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
